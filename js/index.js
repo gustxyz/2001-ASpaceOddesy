cd8607a9a8caaa5465ff1b458e499c415ec9bccd
Padre-Rugi <lifeasalemniscate@gmail.com>
2017-05-01 01:34:51 -0700

    Added Lettering

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 
+//Page Scroller function
 $(document).ready(function() {
 			$('#fullpage').fullpage({
 				verticalCentered: false
 			});
 		});
 
+
+//custom video
+
+//Star animations
 setInterval(function() {
   // method to be executed;
   $("#star-1").css("display", "none");
@@ -46,3 +51,9 @@ setInterval(function() {
   $("#star-3").css("display", "block");
 
 }, 8000);
+
+
+//lettering
+  $(document).ready(function() {
+	    $("#trailer-title").lettering();
+	  });
